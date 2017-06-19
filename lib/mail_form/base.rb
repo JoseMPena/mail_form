fcba9cee17c553f84eb5e09a79346045015aaeb2
JoseMPena <josmpena91@gmail.com>
2017-06-19 09:26:26 +0200

    Capybara integration - GREEN

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -13,6 +13,12 @@ module MailForm
     class_attribute :attribute_names
     self.attribute_names = []
 
+    def initialize(attributes = {})
+      attributes.each do |attr, value|
+        self.public_send("#{attr}=", value)
+      end if attributes
+    end
+
     def deliver
       valid? ? MailForm::Notifier.contact(self).deliver : false
     end
--- mail_form.gemspec
+++ mail_form.gemspec
@@ -19,4 +19,6 @@ Gem::Specification.new do |s|
   s.add_dependency 'rails', '~> 5.0.3'
 
   s.add_development_dependency 'sqlite3'
+  
+  s.add_development_dependency 'capybara'
 end
